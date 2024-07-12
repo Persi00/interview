3000fb7c3b4a094f57c4b8709f8b9f925876b571
Persi00 <franciszek.zachuta@gmail.com>
2024-07-12 14:30:17 +0200

    added tests and formatted json

--- tests/main_test.py
+++ tests/main_test.py
@@ -6,8 +6,8 @@ import pytest
     (
         (("iron-plate", 1), {'Electric mining drill': 2, "Electric furnace": 2}),
         (("uranium-235", 1), {'Centrifuge': 1716, 'Electric mining drill': 5720}),
-        # (("uranium-238", 1), {'uranium-ore': 20}),
-        # (("electronic-circuit", 1), {'copper-ore': 2, 'iron-ore': 1}),
+        (("uranium-238", 1), {'Centrifuge': 24, 'Electric mining drill': 80}),
+        (("electronic-circuit", 1), {'Electric mining drill': 6, "Electric furnace": 6, "Assembling machine 3": 2}),
         # (("speed-module", 1), {'iron-ore': 15, 'copper-ore': 33, 'coal': 5, 'water': 100, 'crude-oil': 200}),
         # (("explosive-uranium-cannon-shell", 1), {'iron-ore': 10, 'coal': 2, 'crude-oil': 100, 'water': 90, 'uranium-ore': 20}),
     )
