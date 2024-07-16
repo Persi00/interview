f7db97ab66adaa59548372d126a036333f3f7b9c
Persi00 <franek.zachuta@op.pl>
2024-07-16 17:54:35 +0200

    small changes

--- tojson.py
+++ tojson.py
@@ -1,53 +1,63 @@
-luaFile = open("data/recipe.lua", "r")
-luaFile.readline()
-linesLua = luaFile.readlines()
-linesLua.pop()
-luaFile.close()
+def readDataFile(path : str) -> list:
+    file = open(path, "r")
+    lines = file.readlines()
+    file.close()
+    return lines
 
-textLua = "".join(linesLua)
-tokens = []
+def dumpJsonFile(path : str, textJson : str):
+    jsonFile = open(path, "w+")
+    jsonFile.writelines(textJson)
+    jsonFile.close()
 
-token = ""
-specialChars = {'{', '}', ','}
-
-def addToken(token):
+def addToken(token, tokens):
     if token != "":
         if token[-1] != '"' and token != "true" and token != "false" and not token[0].isnumeric():
             token = f'"{token}"'
         tokens.append(token)
     token = ""
 
-for char in textLua:
-    if token == "--":
-        if char == '\n':
+def ToJson(linesData : list) -> str:
+    linesData.pop()
+    linesData.pop(0)
+    textData = "".join(linesData)
+    tokens : list = []
+
+    token = ""
+    specialChars = {'{', '}', ','}
+
+    for char in textData:
+        if token == "--":
+            if char == '\n':
+                token = ""
+        elif char == '=':
+            addToken(token, tokens)
             token = ""
-    elif char == '=':
-        addToken(token)
-        token = ""
-        tokens.append(':')
-    elif char in specialChars:
-        addToken(token)
-        token = ""
-        tokens.append(char)
-    elif char != ' ' and char != '\n':
-        token += char
+            tokens.append(':')
+        elif char in specialChars:
+            addToken(token, tokens)
+            token = ""
+            tokens.append(char)
+        elif char != ' ' and char != '\n':
+            token += char
 
-leftBracketsStack = []
+    leftBracketsStack = []
 
-for i in range(len(tokens)):
-    if tokens[i] == '{':
-        tokens[i] = '['
-        leftBracketsStack.append(i)
-    elif tokens[i] == ':' and tokens[leftBracketsStack[-1]] == '[':
-        tokens[leftBracketsStack[-1]] = '{'
-    elif tokens[i] == '}':
-        if tokens[leftBracketsStack[-1]] == '[':
-            tokens[i] = ']'
-        leftBracketsStack.pop()
+    for i in range(len(tokens)):
+        if tokens[i] == '{':
+            tokens[i] = '['
+            leftBracketsStack.append(i)
+        elif tokens[i] == ':' and tokens[leftBracketsStack[-1]] == '[':
+            tokens[leftBracketsStack[-1]] = '{'
+        elif tokens[i] == '}':
+            if tokens[leftBracketsStack[-1]] == '[':
+                tokens[i] = ']'
+            leftBracketsStack.pop()
 
 
-textJson = f'{"".join(tokens)}'.replace(',}', '}').replace(',]', ']')
+    textJson = f'{"".join(tokens)}'.replace(',}', '}').replace(',]', ']')
+    return textJson
 
-jsonFile = open("data/recipe.json", "w+")
-jsonFile.writelines(textJson)
-jsonFile.close()
+if __name__ == "__main__":
+    linesData = readDataFile("data/recipe.lua")
+    textJson = ToJson(linesData)
+    dumpJsonFile("data/recipe.json", textJson)
